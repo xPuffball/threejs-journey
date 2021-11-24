0a854cc3b93edd5548996b93b0a6c42b5a69d081
Jaemin Han <j4emin.han@gmail.com>
2021-11-24 17:15:32 -0500

    add animation section

--- Chapter 1 - Basics/3 - Transform/src/script.js
+++ Chapter 1 - Basics/3 - Transform/src/script.js
@@ -1,1 +1,55 @@
 import './style.css'
+import * as THREE from 'three'
+
+const scene = new THREE.Scene()
+
+
+const stairs = new THREE.Group()
+stairs.rotation.y = Math.PI * 0.25
+scene.add(stairs)
+
+const bottom = new THREE.Mesh(
+    new THREE.BoxGeometry(2, 1, 3),
+    new THREE.MeshBasicMaterial({color: 'red'})
+)
+bottom.position.y = -1;
+stairs.add(bottom)
+
+const middle = new THREE.Mesh(
+    new THREE.BoxGeometry(2, 1, 2),
+    new THREE.MeshBasicMaterial({color: 'blue'})
+)
+middle.position.set(0, 0, -0.5)
+stairs.add(middle)
+
+const top = new THREE.Mesh(
+    new THREE.BoxGeometry(2, 1, 1),
+    new THREE.MeshBasicMaterial({color: 'green'})
+)
+top.position.set(0, 1, -1)
+stairs.add(top)
+
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height)
+camera.position.z = 4;
+camera.position.y = 2;
+camera.lookAt(stairs.position)
+scene.add(camera)
+
+const renderer = new THREE.WebGLRenderer(
+    {canvas: document.querySelector('.webgl')}
+)
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
+
+const tick = () => {
+    stairs.rotation.y = stairs.rotation.y + 0.005
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick)
+}
+
+tick()
---
+++ Chapter 1 - Basics/4 - Animation/src/script.js
@@ -0,0 +1,41 @@
+import './style.css'
+import * as THREE from 'three'
+import { Mesh } from 'three'
+
+const scene = new THREE.Scene()
+
+const box = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 'red'})
+)
+
+scene.add(box)
+
+const sizes = {
+    width: 800,
+    height: 600
+}
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+const renderer = new THREE.WebGLRenderer(
+    {
+        canvas: document.querySelector('.webgl')
+    }
+)
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
+
+const clock = new THREE.Clock()
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+
+    box.rotation.y = elapsedTime
+
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick)
+}
+
+tick()
